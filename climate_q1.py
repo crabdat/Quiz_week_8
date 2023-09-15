91c4b5bbff489d21d7fe833e684e8529f6d72c5c
Atti <atticusburgess@gmail.com>
2023-09-15 12:20:52 +1000

    updated q1

--- climate_q1.py
+++ climate_q1.py
@@ -1,8 +1,19 @@
 import matplotlib.pyplot as plt
-        
+import sqlite3
+
+
 years = []
 co2 = []
 temp = []
+
+connect = sqlite3.connect(r".\climate.db")
+cursor = connect.cursor()
+
+sql_cmd = """
+SELECT years, co2, temp FROM C:/Users/Atticus/PycharmProjects/2810 quiz/Quiz_week_8/climate.db WHERE type='ClimateData';
+"""
+cursor.execute(sql_cmd)
+res = cursor.fetchall()
 
 plt.subplot(2, 1, 1)
 plt.plot(years, co2, 'b--') 
